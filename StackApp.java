972881050a33b13600b2157a533a363a5f828485
Felipe Almeida <felipeortalmeida@gmail.com>
2020-06-11 17:19:19 -0300

    Adjusted code

--- StackApp.java
+++ StackApp.java
@@ -6,16 +6,11 @@ import models.Stack;
 public class StackApp {
     public static void main(String[] args) {
         Stack myStack = new Stack(5);
-
         int i = 5;
         while(!myStack.isStackFilled()) myStack.push(i++);
         System.out.println(myStack);
 
-        // while (!myStack.isStackEmpty()) myStack.pop();
-        // System.out.println(myStack);
-
         Stack newStack = new Stack(5);
-
         System.out.println(myStack.moveTo(newStack));
         System.out.println(myStack);
         System.out.println(newStack);
